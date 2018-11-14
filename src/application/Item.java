a624d59ab4d5848f16af9cd8b7097ca725c66159
Reena <19yuanr@elmbrookstudents.org>
2018-11-14 09:44:26 -0600

    added getters and setters for review and item

--- src/application/Item.java
+++ src/application/Item.java
@@ -18,5 +18,29 @@ public class Item {
 		this.type=type;
 		this.price=price;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	
 }
